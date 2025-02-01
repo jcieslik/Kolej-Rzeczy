3b043711e513c933c5abfefc02d656e17d28b45d
Miklo <miklosher@gmail.com>
2025-02-01 12:17:20 +0100

    aaaaaa 4

---
+++ character_body_2d.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var speed: float = 200.0 
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1  
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1  
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1  
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1  
+		
+	if direction.length() > 0:
+		direction = direction.normalized()
+
+	velocity = direction * speed
+	move_and_slide()
+	
+	if get_slide_collision_count() > 0:
+		velocity = Vector2.ZERO  # 
