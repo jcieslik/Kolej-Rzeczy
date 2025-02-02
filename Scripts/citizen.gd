5262220568ae5765a4c89bd533049d7a453c629e
Kerinjo <konmarciniak@gmail.com>
2025-02-02 13:41:48 +0100

    mieszkancy olsztyna chodza po olsztynie

---
+++ Scripts/citizen.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@export var speed = 80
+@export var moving = false
+
+
+func _process(_delta: float) -> void:
+	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	var direction
+	moving = !moving
+	if moving:
+		var rand = randi()%8
+		match rand:
+			0: direction = Vector2( 1, 0)
+			1: direction = Vector2( 1, 1)
+			2: direction = Vector2( 0, 1)
+			3: direction = Vector2(-1, 1)
+			4: direction = Vector2(-1, 0)
+			5: direction = Vector2(-1,-1)
+			6: direction = Vector2( 0,-1)
+			7: direction = Vector2( 1, 1)		
+	else:
+		direction = Vector2(0, 0)
+	
+	velocity = direction * speed
