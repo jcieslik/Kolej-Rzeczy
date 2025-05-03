d5d5ba1a05aaac7a9efb9585b7b1ff16c11e8650
Jakub Cieślik <jcieslik15@gmail.com>
2025-05-03 16:10:12 +0200

    attacking rat

--- Scripts/obj_machete.gd
+++ Scripts/obj_machete.gd
@@ -37,7 +37,7 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 	
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
-		body.take_damage()
+		body.take_damage(facing_direction)
 
 enum PlayerDirection {
 	UP,
