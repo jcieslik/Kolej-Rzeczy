a6eea453d05891c5fabcb81d568b41d9045c0038
Kerinjo <konmarciniak@gmail.com>
2025-05-03 16:08:48 +0200

    jazda przez dialogi

--- dialogues/Dialogue.gd
+++ dialogues/Dialogue.gd
@@ -4,14 +4,22 @@ extends CanvasLayer
 var dialogue_file: String
 
 var dialogue = []
+var current_dialogue_id = 0
+var dialogue_active = false
 
 func _ready():
+	$TextureRect.visible = false
 	start()
 	
-func start():
-	dialogue = load_dialogue()
 	$TextureRect/type.text = dialogue[0]["type"]
 	$TextureRect/text.text = dialogue[0]["text"]
+	
+func start():
+	if dialogue_active:
+		return
+	dialogue_active = true
+	$TextureRect.visible = true
+	dialogue = load_dialogue()
 	
 func load_dialogue():
 	if FileAccess.file_exists(dialogue_file):
@@ -20,3 +28,21 @@ func load_dialogue():
 		file.close()
 		return JSON.parse_string(text)
 	return null
+	
+func _input(event):
+	if not dialogue_active:
+		return
+	if event.is_action_pressed("action"):
+		next_script()
+		
+func next_script():
+	current_dialogue_id += 1
+	
+	if current_dialogue_id >= len(dialogue):
+		dialogue_active = false
+		$TextureRect.visible = false
+		return
+		
+	$TextureRect/type.text = dialogue[current_dialogue_id]["type"]
+	$TextureRect/text.text = dialogue[current_dialogue_id]["text"]
+		
